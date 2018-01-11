d4a698e7ecb5eb88c09449628b71d0ffc2ea2aa9
daixilong <1113024640@qq.com>
2018-01-11 10:52:31 +0800

    maven多模块的使用

---
+++ test_core/src/main/java/com/xdl/model/UserInfo.java
@@ -0,0 +1,25 @@
+package com.xdl.model;
+
+/**
+ * Created by a on 2018/1/11.
+ */
+public class UserInfo {
+    private String name;
+    private String des;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDes() {
+        return des;
+    }
+
+    public void setDes(String des) {
+        this.des = des;
+    }
+}
---
+++ test_core_app/src/main/java/com/xdl/demo/TestInfo.java
@@ -0,0 +1,10 @@
+package com.xdl.demo;
+
+import com.xdl.model.UserInfo;
+
+/**
+ * Created by a on 2018/1/11.
+ */
+public class TestInfo {
+    UserInfo userInfo=new UserInfo();
+}
